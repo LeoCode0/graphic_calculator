5f760892bf6c14388c2b65b294409e55c6bc1037
Leonardo Espinoza <leoespinozidk@gmail.com>
2021-10-18 00:52:02 -0500

    :art: Improving structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const main = document.querySelector("main");
 
-const xAxis = document.querySelector("#x-axis");
-const yAxis = document.querySelector("#y-axis");
+const xAxisInput = document.querySelector("#x-axis");
+const yAxisInput = document.querySelector("#y-axis");
 
 const buttonSubmit = document.querySelector("#button-submit");
 const buttonClear = document.querySelector("#button-clear");
 buttonClear.disabled = true;
 
-let arrValues = [];
+let arrValuesX = [];
+let arrValuesY = [];
+const arrColors = [
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+];
 
 // Cartesian plane
 const createPlane = () => {
@@ -41,28 +49,31 @@ createPlane();
 // Functions
 
 const drawCartesianPoint = (x, y) => {
-  document.querySelector("#plane").getContext("2d").fillRect(x, -y, 4, 4);
+  const planeContext = document.querySelector("#plane").getContext("2d");
+  let xTextValue = Number(x) + 5;
+  let yTextValue = Number(y) + 5;
+  planeContext.fillStyle = arrColors[arrValuesX.length - 1];
+  planeContext.fillRect(x, -y, 4, 4);
+  planeContext.fillText(`(${x}, ${y})`, xTextValue, -yTextValue);
 };
 
-const drawChart = (arrValues) => {
-  let chartCtx;
+const drawChart = (xValues, yValues) => {
+  let chartCtx = document.querySelector("#graph");
   if (!chartCtx) {
     const canvasChart = document.createElement("canvas");
     const container = document.querySelector(".data");
     canvasChart.id = "graph";
     container.appendChild(canvasChart);
     chartCtx = document.querySelector("#graph");
-  } else {
-    chartCtx = document.querySelector("#graph");
   }
   const myChart = new Chart(chartCtx.getContext("2d"), {
     type: "bar",
     data: {
-      labels: ["X1", "X2", "X3", "X4", "X5"],
+      labels: xValues,
       datasets: [
         {
-          label: "X Values",
-          data: arrValues,
+          label: "X Value",
+          data: yValues,
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
@@ -70,13 +81,7 @@ const drawChart = (arrValues) => {
             "rgba(75, 192, 192, 0.2)",
             "rgba(153, 102, 255, 0.2)",
           ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-          ],
+          borderColor: arrColors,
           borderWidth: 1,
         },
       ],
@@ -92,8 +97,8 @@ const drawChart = (arrValues) => {
 };
 
 const clearValues = () => {
-  xAxis.value = "";
-  yAxis.value = "";
+  xAxisInput.value = "";
+  yAxisInput.value = "";
 };
 
 const clearPlane = () => {
@@ -104,29 +109,39 @@ const clearPlane = () => {
 // Event listeners
 
 buttonSubmit.addEventListener("click", (button) => {
-  const xValue = xAxis.value;
-  const yValue = yAxis.value;
+  const xValue = xAxisInput.value;
+  const yValue = yAxisInput.value;
   if (
     xValue.length > 0 &&
     yValue.length > 0 &&
-    Number(xValue) <= 300 &&
-    Number(yValue) <= 300
+    Number(xValue) <= 250 &&
+    Number(yValue) <= 250 &&
+    Number(xValue) >= -250 &&
+    Number(yValue) >= -250
   ) {
-    arrValues.push(xValue);
-    if (arrValues.length === 5) {
-      button.target.disabled = true;
-      buttonClear.disabled = false;
-      drawChart(arrValues);
-      arrValues = [];
-    }
+    arrValuesX.push(xValue);
+    arrValuesY.push(yValue);
     drawCartesianPoint(xValue, yValue);
     clearValues();
+    if (arrValuesX.length === 5) {
+      button.target.disabled = true;
+      xAxisInput.disabled = true;
+      yAxisInput.disabled = true;
+      buttonClear.disabled = false;
+      drawChart(arrValuesY, arrValuesX);
+      arrValuesX = [];
+      arrValuesY = [];
+    }
   }
 });
 
 buttonClear.addEventListener("click", (button) => {
   button.target.disabled = true;
   buttonSubmit.disabled = false;
+
+  xAxisInput.disabled = false;
+  yAxisInput.disabled = false;
+
   document.querySelector("#graph").remove();
   clearPlane();
 });
